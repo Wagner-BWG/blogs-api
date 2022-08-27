54a7695f0f2f4980b98d74caa9a1499763f2d0f1
Wagner B <wagner_mbarbosa@hotmail.com>
2022-08-27 15:46:11 -0300

    :new:FEAT: Implementado o endpoint POST /login

---
+++ src/controllers/loginController.js
@@ -0,0 +1,11 @@
+const loginService = require('../services/loginService');
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  
+  const response = await loginService(email, password);
+  
+  return res.status(response.status).json(response.json);
+};
+
+module.exports = login;
---
+++ src/services/loginService.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../database/models');
+
+const secret = process.env.JWT_SECRET;
+
+const findUser = async (email) => {
+  const response = await User.findAll({
+    where: {
+      email,
+    },
+  });
+  return response;
+};
+
+const login = async (email, password) => {
+  if (!email || !password) {
+    return { status: 400, json: { message: 'Some required fields are missing' } };
+  }
+  const response = await findUser(email);
+
+  if (response.length === 0) return { status: 400, json: { message: 'Invalid fields' } };
+
+  const data = response[0].dataValues;
+  if (data.password !== password) {
+    return { status: 400, json: { message: 'Invalid fields' } };
+  }
+
+  const jwtConfig = {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  };
+
+  const token = jwt.sign({ data: email }, secret, jwtConfig);
+  
+  return { status: 200, json: { token } };
+};
+
+module.exports = login;
