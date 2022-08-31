684e4e9749372b9d243516e28cfc5ce8b56b57be
Wagner B <wagner_mbarbosa@hotmail.com>
2022-08-31 15:26:43 -0300

    :bangbang:FEAT: implementado o endpoint POST /post

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,12 +1,23 @@
-const { validateInputFields } = require('../services/postService');
+const { validateInputFields,
+  validateCategory, composeNewPost } = require('../services/postService');
 
 const newPost = async (req, res) => {
   const { title, content, categoryIds } = req.body;
+  const { user } = req;
   if (!validateInputFields(title, content, categoryIds)) {
     return res.status(400).json({
       message: 'Some required fields are missing',
     });
-  } return res.status(200).json({ message: 'ok ' });
+  }
+  const categoryIsValid = await validateCategory(categoryIds);
+  if (!categoryIsValid) {
+    return res.status(400).json({
+      message: '"categoryIds" not found',
+    });
+  }
+  const { status, json } = await composeNewPost(user, title, content, categoryIds);
+  
+  return res.status(status).json(json);
 };
 
 module.exports = { newPost };
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,11 +1,27 @@
+const { BlogPost, Category, PostCategory } = require('../database/models');
+
 const validateInputFields = (title, content, categoryIds) => {
   const tests = [title, content, categoryIds];
   let testResult = true;
   tests.forEach((test) => {
-    console.log(test);
     if (!test) testResult = false;
   });
   return testResult;
 };
 
-module.exports = { validateInputFields };
+const validateCategory = async (categoryIds) => {
+  const category = await Category.findAll({ where: { id: categoryIds } });
+  if (category.length !== categoryIds.length) return false;
+  return true;
+};
+
+const composeNewPost = async (user, title, content, categoryIds) => {
+  const newPost = await BlogPost.create({ userId: user.id, title, content });
+  console.log(typeof categoryIds);
+  categoryIds.forEach(async (ctgId) => {
+    await PostCategory.create({ postId: newPost.id, categoryId: ctgId }, { fields: ['postId', 'categoryId'] });
+  });
+  return { status: 201, json: newPost };
+};
+
+module.exports = { validateInputFields, validateCategory, composeNewPost };
