cbf415dd6e2158ea04ac06657fa52f60ed77b1e7
Wagner B <wagner_mbarbosa@hotmail.com>
2022-08-31 15:30:12 -0300

    :wrench:FIX: corrigido erro de lint

--- src/services/postService.js
+++ src/services/postService.js
@@ -19,7 +19,9 @@ const composeNewPost = async (user, title, content, categoryIds) => {
   const newPost = await BlogPost.create({ userId: user.id, title, content });
   console.log(typeof categoryIds);
   categoryIds.forEach(async (ctgId) => {
-    await PostCategory.create({ postId: newPost.id, categoryId: ctgId }, { fields: ['postId', 'categoryId'] });
+    await PostCategory.create(
+      { postId: newPost.id, categoryId: ctgId }, { fields: ['postId', 'categoryId'] },
+    );
   });
   return { status: 201, json: newPost };
 };
